71734461946ebf53a062da477c32270640b4a93a
Daseanle <daseanleelee@gmail.com>
2025-12-09 17:03:42 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // <--- 关键是这一行，指明了app文件夹
   ],
   theme: {
     extend: {},
