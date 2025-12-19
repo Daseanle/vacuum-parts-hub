bca15c99428416b9dc4901141cca4412a99e5501
Daseanle <daseanleelee@gmail.com>
2025-12-19 18:21:42 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 强制去除尾部斜杠 (比如 /guide/dyson-v8/ 会跳到 /guide/dyson-v8)
+  trailingSlash: false, 
+};
 
 export default nextConfig;
